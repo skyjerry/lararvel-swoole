ed573c7dae60fa50f5514bc9d91e7a3d41ed20cf
skyjerry <ldwangzhen@gmail.com>
2018-05-25 22:28:55 +0800

    init server

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Laravel on Swoole
+* goals
+	* Try to run Laravel normally on swoole 
+	* Optimize code
+	* Make a package
+	* ..
+
+* todos
+	* Resolve the Notice about argv
+	* Make a Container for swoole
+	* ..
---
+++ server.php
@@ -0,0 +1,70 @@
+<?php
+
+$http = new swoole_http_server("127.0.0.1", 9501);
+
+// $http->set(
+// 	[
+
+// 	]
+// );
+
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+// $http->on('WorkerStart', function($server, $worker_id) {
+	
+	
+// });
+
+$http->on('request', function ($request, $response) use($app, $kernel) {
+
+	$_SERVER = [];
+	$_GET = [];
+	$_POST = [];
+
+	if (isset($request->server)) {
+        foreach ($request->server as $k => $v) {
+            $_SERVER[$k] = $v;
+        }
+    }
+
+    if (isset($request->header)) {
+        foreach ($request->header as $k => $v) {
+            $_SERVER[$k] = $v;
+        }
+    }
+
+    if (isset($request->get)) {
+        foreach ($request->get as $k => $v) {
+            $_GET[$k] = $v;
+        }
+    }
+
+    if (isset($request->post)) {
+        foreach ($request->post as $k => $v) {
+            $_POST[$k] = $v;
+        }
+    }
+
+	ob_start();
+
+	$response_laravel = $kernel->handle(
+	    $request_laravel = Illuminate\Http\Request::capture()
+	);
+
+	$response_laravel->send();
+
+	$kernel->terminate($request_laravel, $response_laravel);
+
+	$result = ob_get_contents();
+
+	ob_clean();
+
+    $response->end(print_r($_SERVER));
+});
+
+$http->start();
